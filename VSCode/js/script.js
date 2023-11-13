fdd41b9c9c3826cc36be9e05e864d55514c8ed0c
IM5User <marius.wickli@stud.fhgr.ch>
2023-11-13 11:26:21 +0100

    Buttons Styling

--- VSCode/js/script.js
+++ VSCode/js/script.js
@@ -1,14 +1,22 @@
+
+// Collapsible buttons von AI Tools
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+
+    //Die Klasse collapsible des Buttons im HTML wird auf einen Klick abgehört
+    coll[i].addEventListener("click", function() {
+
+        //Klasse active hinzufügen um sie mittels CSS anzusprechen
+        this.classList.toggle("active");
+
+        //Anzeige Logik
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
 }
